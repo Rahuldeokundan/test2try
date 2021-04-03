e4b9c86f9dd2b3cef556ee28831d4660e987e88d
Rahul Deo Kundan <57994358+Rahuldeokundan@users.noreply.github.com>
2021-04-03 13:19:33 +0530

    Update settings.py

--- test2try/settings.py
+++ test2try/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"web", "static"),
+]
+STATIC_ROOT  = os.path.join(BASE_DIR, "static")
